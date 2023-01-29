6f42ee7ab956613db7d3f989d723b8b1a8e9a801
Waqas K <waqas.k@tricension.com>
2023-01-29 16:51:17 +0500

    Initial Commit

---
+++ GrocessaryApp/AbstractClass/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrocessaryApp.AbstractClass
+{
+    //it is incomplete class
+    internal abstract class Employee
+    {
+        public int Id;
+        public string Name;
+        public string Country;
+        public Employee()
+        {
+            Console.WriteLine("Employee Parent Class Constructor");
+        }
+
+        
+
+        public void GetEmployeeDetail()
+        {
+            Console.WriteLine($"Id = {Id} Name = {Name} and Country = {Country}");
+        }
+    }
+}
